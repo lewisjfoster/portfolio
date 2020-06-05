0a5d08b200068a7615cf4ee9c9f0a84379269cdb
Lewis Foster <lewis.foster@clearscore.com>
2020-06-05 10:44:05 +0100

    fix: jest config for svgs

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
     setupFiles: ['<rootDir>/scripts/setupTests.js'],
     moduleNameMapper: {
         '\\.(css|less|scss|sss|styl)$': '<rootDir>/node_modules/jest-css-modules',
-        '^.+\\.svg$': 'jest-svg-transformer',
+        '^.+\\.svg$': '<rootDir>/node_modules/jest-svg-transformer',
     },
 };
